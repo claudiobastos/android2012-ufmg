b320af90564588b2e0572203d8a627721dcbf733
Claudio Bastos <claudio@pathfinder.(none)>
2012-07-26 17:44:35 -0300

    Primeiro commit

---
+++ src/com/dcc052/ufmg2012/moreaqui/MoreAqui.java
@@ -0,0 +1,24 @@
+package com.dcc052.ufmg2012.moreaqui;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class MoreAqui extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    
+}
